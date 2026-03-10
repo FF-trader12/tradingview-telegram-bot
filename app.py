8b57448dc344e43ec361c4e1b7ef7b1494362967
FF-trader12 <adamgrundy7@gmail.com>
2026-03-10 23:29:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -982,3 +982,8 @@ def telegram_webhook():
 
     except Exception as e:
         return jsonify({"ok": False, "error": str(e)}), 500
+        
+        
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
